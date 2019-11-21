37ac83a510a5307455e3035b565a0b797835b03f
= <atulsingh1396@gmail.com>
2019-11-22 02:46:40 +0530

    Added grid props depending on the image fetched

--- src/components/Imagecard.js
+++ src/components/Imagecard.js
@@ -1,10 +1,30 @@
 import React from "react";
 
 class Imagecard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      span: 0
+    };
+
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpan);
+  }
+  setSpan = () => {
+    const height = this.imageRef.current.clientHeight;
+    const span = Math.ceil(height / 10) + 1;
+    this.setState({ span });
+  };
+
   render() {
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.span}` }}>
         <img
+          ref={this.imageRef}
           src={this.props.image.urls.regular}
           alt={this.props.image.description}
         />
